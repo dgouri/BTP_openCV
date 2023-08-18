90c7312ff37ddd12cb19bf4f0b22590d06820b73
dgouri <95135687+dgouri@users.noreply.github.com>
2023-08-19 03:23:34 +0530

    Update coordinates.py

--- coordinates.py
+++ coordinates.py
@@ -1,1 +1,17 @@
+//this code gives average of all black points coordinates (using pixel....)
+import numpy as np
+import cv2
 
+img = cv2.imread('Desktop/img3.jpg')
+
+x, y, z = np.where(img == (0, 0, 0))
+points = list(zip(x, y))
+
+if points:
+    points_array = np.array(points)
+    average_x = np.mean(points_array[:, 0])
+    average_y = np.mean(points_array[:, 1])
+    print("Average X coordinate:", average_x)
+    print("Average Y coordinate:", average_y)
+else:
+    print("No points found with the specified color.")
